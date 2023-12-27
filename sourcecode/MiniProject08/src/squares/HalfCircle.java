5f161c0ea7b57ed6f8c16690ff4c1a4e72d46b93
Vu Duc Thinh 20210816 <thinh.vd210816@sis.hust.edu.vn>
2023-12-27 18:21:51 +0700

    Add some features

--- sourcecode/MiniProject08/src/squares/HalfCircle.java
+++ sourcecode/MiniProject08/src/squares/HalfCircle.java
@@ -1,7 +1,21 @@
 package squares;
-
+import gem.*;
 public class HalfCircle extends Squares{
-		public HalfCircle(int id) {
+	private int numberOfBigGems = 1;	
+	private int numberOfSmallGems = 0;	
+	public HalfCircle(int id) {
 		super(id);
-		}
+	}
+	
+	public int getPoint() {
+		BigGem bigGem = new BigGem();
+		SmallGem smallGem = new SmallGem();
+		int point = numberOfBigGems*bigGem.getPoint() +
+				numberOfSmallGems*smallGem.getPoint();
+		numberOfBigGems = 0;	
+		numberOfSmallGems = 0;	
+		return point;
+	}
+	
+	
 }
--- sourcecode/MiniProject08/src/squares/Squares.java
+++ sourcecode/MiniProject08/src/squares/Squares.java
@@ -1,37 +1,27 @@
 package squares;
-import java.util.*;
+
 import gem.*;
 
 public abstract class Squares {
+	public int point;
 	private int id;
-	public static final int MAX_POINTS = 60;
-	private ArrayList<Gem> squares = new ArrayList<Gem>();
-	
+	private int numberOfSmallGems;
+
+
 	public Squares(int id) {
 		this.id = id;
 	}
 	
-	public int getPosition() {
-		while (id < 0) {
-			id += 12;
-		}
+	public int getId() {
 		return id % 12;
 	}
-
 	public int getPoint() {
-		int sum = 0;
-		for (Gem gem:squares) {
-			sum += gem.getPoint();
-		}
-		return sum;
+		return point;
 	}
 	
-	public void addGem(Gem gem) {
-		squares.add(gem);
+	public void spreadGems() {
+		numberOfSmallGems++;
 	}
-	public void removeGem() {
-		while (squares.size() !=0) {
-			squares.remove(0);
-		}
-	}
+	
+
 }
