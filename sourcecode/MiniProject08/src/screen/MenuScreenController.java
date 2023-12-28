a485bcfb46a446b6dfe9aac524d2e6049059c560
anhthunguyenngoc <anhthunguyenngoc80@gmail.com>
2023-12-28 10:36:51 +0700

    add screen-design-v0.1

---
+++ sourcecode/MiniProject08/src/screen/MenuScreenController.java
@@ -0,0 +1,28 @@
+package screen;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+public class MenuScreenController { 
+	@FXML
+	Button playBtn;
+	@FXML
+	AnchorPane rootAnchorPane;
+	
+	@FXML
+	AnchorPane playAnchorPane;
+	
+	public MenuScreenController() {	  }
+		@FXML		
+		
+		private void initialize() {
+
+		}
+		
+	    @FXML
+	    void playBtnClicked(ActionEvent event) {
+
+	    }
+}
