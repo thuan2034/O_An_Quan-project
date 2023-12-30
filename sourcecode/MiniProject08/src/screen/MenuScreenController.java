8cbf8a6edb0f45624ebb5fe56cf078346c212bfe
anhthunguyenngoc <anhthunguyenngoc80@gmail.com>
2023-12-30 16:58:47 +0700

    update rule.fxml

--- sourcecode/MiniProject08/src/screen/MenuScreenController.java
+++ sourcecode/MiniProject08/src/screen/MenuScreenController.java
@@ -1,14 +1,9 @@
 package screen;
 
-import java.io.IOException;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
 
 public class MenuScreenController { 
 	@FXML
