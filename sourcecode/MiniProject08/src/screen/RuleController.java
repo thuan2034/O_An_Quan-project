fa7e7072c894fc438aab32ae53b33476bedc74d5
HgThuan <115547450+HgThuan@users.noreply.github.com>
2024-01-04 15:04:00 +0700

    Update RuleController.java

--- sourcecode/MiniProject08/src/screen/RuleController.java
+++ sourcecode/MiniProject08/src/screen/RuleController.java
@@ -1,7 +1,24 @@
 package screen;
-
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 public class RuleController {
 
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private Button playButton;
+
+    @FXML
+    private void handleBackButtonAction(ActionEvent event) {
+        System.out.println("Back button clicked");
+    }
+
+    @FXML
+    private void handlePlayButtonAction(ActionEvent event) {
+        System.out.println("Play button clicked");
+    }
     
 
 }
